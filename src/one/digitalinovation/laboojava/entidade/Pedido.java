ba14dd19ea4306119a9e279a6866e0e6656427ad
Angelo Antonio <angelio123@hotmail.com>
2023-09-24 21:22:12 -0300

    Update Pedido.java
    Implementação da classe Pedido.

--- src/one/digitalinovation/laboojava/entidade/Pedido.java
+++ src/one/digitalinovation/laboojava/entidade/Pedido.java
@@ -9,10 +9,57 @@ import java.util.List;
  */
 public class Pedido {
 
-    //TODO Preencher esta classe
+    private String codigo;
 
-    //codigo
-    //cliente
-    //produtos
-    //total
+    private Cliente cliente;
+
+    private List<Produto> produtos;
+
+    private double total;
+
+    public Pedido(){
+        this.produtos = new ArrayList<>();
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public Cliente setCliente(){
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProduto(){
+        return produtos;
+    }
+
+    public void setProduto(List<Produto> produtos){
+        this.produtos = produtos;
+    }
+
+    public double getTotal(){
+        return total;
+    }
+
+    public void setTotal(double total){
+        this.total = total;
+    }
+
+    @Override
+    public String toString(){
+        return "Pedido{" +
+                "codigo='" + codigo + '\'' +
+                ", cliente='" + cliente +
+                ", produtos='" + getProdutosComprados() +
+                ", total='" + total +
+                '}';
+    }
 }
