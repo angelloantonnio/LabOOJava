f1b547d5d540788403ff8e78ba8aa2efa2c10506
Angelo Antonio <angelio123@hotmail.com>
2023-09-26 17:52:12 -0300

    Update ProdutoNegocio.java
    Implementação do método excluir, para excluir produtos cadastrados percorrendo o banco de dados.

--- src/one/digitalinovation/laboojava/negocio/ProdutoNegocio.java
+++ src/one/digitalinovation/laboojava/negocio/ProdutoNegocio.java
@@ -54,7 +54,23 @@ public class ProdutoNegocio {
      * @param codigo Código de cadastro do produto
      */
     public void excluir(String codigo) {
-        //TODO Implementar a exclusão
+        
+        int produtoExclusao = -1;
+        for(int i = 0; i < bancoDados.getProdutos().length; i++){
+
+            Produto produto = bancoDados.getProdutos()[i];
+            if(produto.getCodigo().equals(codigo)){
+                produtoExclusao = i;
+                break;
+            }
+        }
+
+        if(produtoExclusao != -1){
+            bancoDados.removerProduto(produtoExclusao);
+            System.out.println("Produto excluído com sucesso.");
+        }else{
+            System.out.println("Produto inexistente.");
+        }
     }
 
     /**
